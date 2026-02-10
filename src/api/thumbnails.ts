6177f9967c83f9b284fbddab8d85eed336650321
jeffschoe <ratacame@protonmail.com>
2026-02-09 22:17:21 -0500

    CH1: File Storage, L7: Using the Filesystem

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -4,6 +4,7 @@ import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import { getAssetDiskPath, getAssetURL, mediaTypeToExt } from "./assets";
 
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
@@ -15,11 +16,17 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const token = getBearerToken(req.headers);
   const userID = validateJWT(token, cfg.jwtSecret);
 
-  console.log("uploading thumbnail for video", videoId, "by user", userID);
+  const video = getVideo(cfg.db, videoId);
+  if(!video) {
+    throw new NotFoundError("Video not found");
+  }
+  if (video.userID !== userID) {
+    throw new UserForbiddenError(
+      `User: ${userID} is not authorized to update this video`
+    );
+  }
 
-  // TODO: implement the upload here
   const formData = await req.formData();
-
   const file = formData.get("thumbnail");
   if (!(file instanceof File)) {
     throw new BadRequestError('Thumbnail file missing')
@@ -37,28 +44,16 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   if (!mediaType) {
     throw new BadRequestError("Missing Content-Type for thumbnail");
   }
+  
+  const ext = mediaTypeToExt(mediaType);
+  const filename = `${videoId}${ext}`;
 
-  const fileData = await file.arrayBuffer();
-  if (!fileData) {
-    throw new Error("Error reading file data");
-  }
+  const assetDiskPath = getAssetDiskPath(cfg, filename);
+  await Bun.write(assetDiskPath, file);
 
-  let video = getVideo(cfg.db, videoId);
-  if(!video) {
-    throw new NotFoundError("Video not found");
-  }
-
-  if (video.userID !== userID) {
-    throw new UserForbiddenError(
-      `User: ${userID} is not authorized to update this video`
-    );
-  }
-
-  const base64Encoded = Buffer.from(fileData).toString("base64");
-  const base64DataURL = `data:${mediaType};base64,${base64Encoded}`;
-
-  video.thumbnailURL = base64DataURL;
-
+  const urlPath = getAssetURL(cfg, filename);
+  video.thumbnailURL = urlPath;
+  
   updateVideo(cfg.db, video);
 
   return respondWithJSON(200, video);
