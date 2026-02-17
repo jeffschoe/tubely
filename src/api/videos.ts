69c796e58c364c379b24d5d5c95d4e44b2154389
jeffschoe <ratacame@protonmail.com>
2026-02-16 21:16:27 -0500

    CH3: AWS S3, L7: SDKs and S3

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -1,8 +1,64 @@
+import { rm } from "fs/promises";
+import path from "path";
+import { getBearerToken, validateJWT } from "../auth";
+import { getVideo, updateVideo } from "../db/videos";
 import { respondWithJSON } from "./json";
-
+import { uploadVideoToS3 } from "../s3";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 import { type ApiConfig } from "../config";
-import type { BunRequest } from "bun";
+import { type BunRequest } from "bun";
+
+
+
+
 
 export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
-  return respondWithJSON(200, null);
+  const MAX_UPLOAD_SIZE = 1 << 30; // 1GB, bitshifted
+
+  const { videoId } = req.params as { videoId?: string };
+  if (!videoId) {
+    throw new BadRequestError("Invalid video ID");
+  }
+
+  const token = getBearerToken(req.headers);
+  const userID = validateJWT(token, cfg.jwtSecret);
+
+  const video = getVideo(cfg.db, videoId);
+  if(!video) {
+    throw new NotFoundError("Video not found");
+  }
+  if (video.userID !== userID) {
+    throw new UserForbiddenError(
+      `User: ${userID} is not authorized to update this video`
+    );
+  }
+
+  const formData = await req.formData();
+  const file = formData.get("video");
+  if (!(file instanceof File)) {
+    throw new BadRequestError('Video file missing')
+  }
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError(
+      `Video file exceeds the maximum allowed size of 1GB`
+    );
+  }
+  const mediaType = file.type;
+  if (mediaType !== "video/mp4") {
+    throw new BadRequestError("Invalid file type. Only MP4 allowed.");
+  }
+
+  const tempFilePath = path.join("/tmp", `${videoId}.mp4`);
+  await Bun.write(tempFilePath, file);
+
+  let key = `${videoId}.mp4`;
+  await uploadVideoToS3(cfg, key, tempFilePath, "video/mp4");
+
+  const videoURL = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${key}`;
+  video.videoURL = videoURL;
+  updateVideo(cfg.db, video);
+
+  await Promise.all([rm(tempFilePath, { force: true })]);
+
+  return respondWithJSON(200, video);
 }
